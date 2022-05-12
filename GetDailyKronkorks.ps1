58eb4ef41c581cc012f9737b728244e2e1e57cba
Julius T <47179422+J-C-O@users.noreply.github.com>
2022-05-12 12:40:44 +0200

    Add files via upload

---
+++ GetDailyKronkorks.ps1
@@ -0,0 +1,27 @@
+﻿param(
+    [string] $EndDate = "2022-06-30",
+    [int] $KronkorkCount,
+    [int] $RequiredKronkorks = 6000
+)
+
+[string] $today = Get-Date -Format "yyyy-MM-dd"
+
+[int] $availableDays = (New-TimeSpan -Start $today -End $EndDate).Days
+
+[int] $kronKorkDiff = $RequiredKronkorks - $KronkorkCount
+
+[double] $kronKorksPerDay = $kronKorkDiff / $availableDays
+[string] $asciiBottle = @'
+                   _
+                  |=|
+                  | |
+                  | |
+                 /   \
+                .     .
+                |-----|
+                |FELDI|
+                |-----|
+                |_____|
+'@
+Write-Host $asciiBottle -ForegroundColor Red
+Write-Host "In den nächsten $($availableDays) Tagen müssen $($kronKorksPerDay) Kronkörks gesammelt werden." -ForegroundColor White -BackgroundColor Red
